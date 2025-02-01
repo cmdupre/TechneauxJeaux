3c680890d203199338b516441d6fc2d2338ac9ff
CMD <caseydupre@gmail.com>
2025-02-01 10:41:58 -0600

    refactor

--- src/main.h
+++ src/main.h
@@ -1,4 +1,15 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include "jutta_bt_proto/CoffeeMakerLoader.hpp"
+
+class Main
+{
+public:
+    void Geaux();
+    static void JoeChanged(const std::shared_ptr<jutta_bt_proto::Joe>& joe);
+    static void AlertsChanged(const std::vector<const jutta_bt_proto::Alert*>& alerts);
+    static void ProductStatisticCountersChanged(const std::shared_ptr<jutta_bt_proto::Joe>& joe);
+};
+
 #endif//_MAIN_H
